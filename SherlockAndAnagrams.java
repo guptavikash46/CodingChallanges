a2b52dbd2604f5c5b70e09d0ae0538131ded23d1
guptavikash46 <guptavikash46@gmail.com>
2019-07-27 00:19:49 +0530

    Added one file

---
+++ SherlockAndAnagrams.java
@@ -0,0 +1,33 @@
+package source;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SherlockAndAnagrams {
+
+    public static void main(String[] args) {
+        String s1 = "abba";
+        char[] newOne = s1.toCharArray();
+        List<String> list = new ArrayList<>();
+        //int[] alphaPos = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        //System.out.println(s1.indexOf("b"));
+        //StringBuffer sb = new StringBuffer();
+        int len = s1.length();
+//        while (len - 1 > 0) {
+//            char a = newOne[0];
+//            for (int i = 0; i < s1.length() - 1; i++) {
+//                newOne[i] = newOne[i + 1];
+//            }
+//            newOne[newOne.length - 1] = a;
+//            for (int u = 0; u< newOne.length; u++)System.out.print(newOne[u]);
+//            System.out.println();
+//            len -= 1;
+//        }
+        for(int i = 1; i<= len; i++ ){
+
+        }
+
+
+    }
+
+}
